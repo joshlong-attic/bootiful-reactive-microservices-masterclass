f5f946182adc206926a141829423c9774d23e748
Josh Long <josh@joshlong.com>
2019-04-23 01:55:10 -0700

    prducer and consmer

---
+++ testing/consumer/src/test/java/rsb/testing/consumer/StubRunnerCustomerClientTest.java
@@ -0,0 +1,52 @@
+package rsb.testing.consumer;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.cloud.contract.stubrunner.spring.StubRunnerPort;
+import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+@SpringBootTest( //
+		webEnvironment = SpringBootTest.WebEnvironment.MOCK, //
+		classes = ConsumerApplication.class //
+)
+@RunWith(SpringRunner.class)
+@Log4j2
+@DirtiesContext
+@AutoConfigureStubRunner(//
+		ids = StubRunnerCustomerClientTest.PRODUCER_ARTIFACT_ID, // <1>
+		stubsMode = StubRunnerProperties.StubsMode.LOCAL  
+)
+public class StubRunnerCustomerClientTest {
+
+	final static String PRODUCER_ARTIFACT_ID = "com.example:producer";
+
+	@Autowired
+	private CustomerClient client;
+
+	@StubRunnerPort(StubRunnerCustomerClientTest.PRODUCER_ARTIFACT_ID)
+	private int portOfProducerService; 
+
+	@Test
+	public void getAllCustomers() {
+
+		var base = "localhost:" + this.portOfProducerService;
+		this.client.setBase(base);
+		log.info("setBase( " + base + ')');
+
+		Flux<Customer> customers = this.client.getAllCustomers();
+		StepVerifier //
+				.create(customers) //
+				.expectNext(new Customer("1", "Jane")) //
+				.expectNext(new Customer("2", "John")) //
+				.verifyComplete();
+	}
+
+}
---
+++ testing/producer/src/main/java/rsb/testing/producer/Customer.java
@@ -0,0 +1,18 @@
+package rsb.testing.producer;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Customer {
+
+	private String id;
+
+	private String name;
+
+}
